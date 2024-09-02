c652483b1c2c7822609514e9efd495fc8cbd0042
andreiagbisit <agbisit.andreijoaqhimali@auf.edu.ph>
2024-09-02 12:19:08 +0800

    'Street' field fix, 09-02-2024

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,7 @@ class User extends Authenticatable
         'bdate',
         'email',
         'contact_no',
+        'street',
         'house_blk_no',
         'house_lot_no',
         'password',

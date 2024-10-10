11cc092289ceada2190e3af46a34a8208a979625
Jio-Detros <detros.jiorhey@auf.edu.ph>
2024-10-10 22:34:52 +0800

    correct sidebar with proper access control on user types

--- resources/views/components/sidebar-base.blade.php
+++ resources/views/components/sidebar-base.blade.php
@@ -1,16 +1,20 @@
 <!-- Sidebar -->
 <ul class="navbar-nav sidebar sidebar-dark accordion" id="accordionSidebar" style="background-color: #f2aa00;">
 
-    {{ $sidebar_landing_link_super_admin }}
-    {{ $sidebar_landing_link_admin }}
-    {{ $sidebar_landing_link_user }}
+    <!-- Sidebar Links Based on Account Type -->
+    @if (auth()->user()->account_type_id == 1)
+        <x-sidebar-landing-link-super-admin></x-sidebar-landing-link-super-admin>
+        <x-sidebar-content-super-admin></x-sidebar-content-super-admin>
+    @elseif (auth()->user()->account_type_id == 2)
+        <x-sidebar-landing-link-admin></x-sidebar-landing-link-admin>
+        <x-sidebar-content-admin></x-sidebar-content-admin>
+    @elseif (auth()->user()->account_type_id == 3)
+        <x-sidebar-landing-link-user></x-sidebar-landing-link-user>
+        <x-sidebar-content-user></x-sidebar-content-user>
+    @endif
 
     <!-- Divider -->
     <hr class="sidebar-divider my-0">
-
-    {{ $sidebar_content_super_admin }}
-    {{ $sidebar_content_admin }}
-    {{ $sidebar_content_user }}
 
     <!-- Divider -->
     <hr class="sidebar-divider d-none d-md-block">

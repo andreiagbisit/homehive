213410b5af4a25fbcf00a6f7bd75fc3eadc0d6b8
Jio-Detros <127663136+Jio-Detros@users.noreply.github.com>
2024-10-30 01:01:07 +0800

    removed admin logs box for superadmin

--- resources/views/acc-mgmt/edit-entry-role.blade.php
+++ resources/views/acc-mgmt/edit-entry-role.blade.php
@@ -110,14 +110,6 @@
                             </div>
                         </div>
                     </div>
-
-                    <div class="card shadow mb-4">
-                        <div class="card-header py-3">
-                            <h6 id="card-h6" class="m-0 font-weight-bold">Admin Logs</h6>
-                        </div>
-
-                        <div class="card-body">
-                            
                 </div>
             </div>
         </div>
